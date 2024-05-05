142f3d1a882a9fcf81cb551fb4e2dcc7a17fe3df
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2024-05-05 12:00:19 +0530

    payment edited successfulyy

--- paymentRoute.py
+++ paymentRoute.py
@@ -39,7 +39,6 @@ def new_payment():
     
 
 @app.route('/payment_list', methods=['GET'])
-@app.route('/payment_list', methods=['GET'])
 @login_required
 def payment_list():
     # Execute the query to fetch payments along with their associated account and category names
@@ -65,3 +64,50 @@ def delete_payment(payment_id):
     else:
         session['error'] = 'Payment not found'
     return redirect(url_for('payment_list'))
+
+
+# @app.route('/edit_payment/<int:payment_id>', methods=['GET'])
+# @login_required
+# def edit_payment(payment_id):
+#     payment = Payment.query.get(payment_id)
+#     accounts = Account.query.all()
+#     categories = Category.query.all()
+    
+#     return render_template('edit_payment.html', payment=payment,accounts=accounts,categories=categories)
+
+
+@app.route('/edit_payment/<int:payment_id>', methods=['GET', 'POST'])
+@login_required
+def edit_payment(payment_id):
+    payment = Payment.query.get(payment_id)
+    accounts = Account.query.all()
+    categories = Category.query.all()
+
+    if request.method == 'POST':
+        # Get the updated data from the form
+        pdate = request.form.get('pdate')
+        account_id = request.form.get('account')
+        amount = request.form.get('amount')
+        category_id = request.form.get('category')
+        to = request.form.get('to')
+        note = request.form.get('note')
+
+        # Update the payment object with the new data
+        payment.date = pdate
+        payment.account = account_id
+        payment.amount = amount
+        payment.category = category_id
+        payment.to = to
+        payment.note = note
+
+        # Commit the changes to the database
+        db.session.commit()
+
+        # Redirect to a success page or somewhere else
+        session['success'] = 'Payment updated successfully'
+        return redirect(url_for('edit_payment', payment_id=payment_id))
+
+    # Render the template with the payment details
+    payment.category = int(payment.category)
+    payment.account = int(payment.account)
+    return render_template('edit_payment.html', payment=payment, accounts=accounts, categories=categories)
