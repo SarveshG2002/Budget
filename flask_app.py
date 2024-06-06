46401699900d6c753d6516c7f6e5a1377db2f88b
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2024-06-06 23:28:40 +0530

    signup page created

--- flask_app.py
+++ flask_app.py
@@ -24,5 +24,5 @@ from accountsRoute import *
 from categoryRoute import *
 
 if __name__ == '__main__':
-    app.run(host='192.168.0.107', port=8080)
+    app.run(host='192.168.0.108', port=8080)
     # app.run()
